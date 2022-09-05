e3bf65c776be6df2826d4947ce4f6fd9534adf39
Jose Rodrigues <rodri651@msu.edu>
2022-09-05 14:54:47 -0400

    Add files via upload

---
+++ lyvset.py
@@ -0,0 +1,17 @@
+##QUAST http://bioinf.spbau.ru/quast  https://github.com/ablab/quast
+import os
+import urllib
+import re
+
+Genomes_file = open( "/mnt/gs18/scratch/groups/manninglab/ST982.txt" ) #Text file with all the genomes to be used in this pipeline. File is used instead of a directory in order to easily edit which genomes are utilized
+Genomes_pair = Genomes_file.readlines() #Here you are reading all of the file names within a specific folder readlines enables this fx 
+
+for i in Genomes_pair: #Here we are stripping tabs and spaces from file names 
+	i = i.rstrip("\n")
+	i = i.rstrip("\r") 
+	print (i)
+	pairGenome = i.split("\t")  
+	RefGenome = str(pairGenome[0])  #Here we place all of the integers within our file that we read and stripped into an array. In this case each TW# Will be in an array that is called RefGenome
+	os.system("singularity run /opt/software/lyveset/lyveset.1.1.4f.sif shuffleSplitReads.pl --numcpus 8 -o /mnt/gs18/scratch/groups/manninglab/ST982/" +RefGenome+ "/interleaved /mnt/gs18/scratch/groups/manninglab/ST982/" +RefGenome+ "/*fastq.gz")
+	print(RefGenome + "_DONE")
+## QUAST command is quast.py (don't need location as long as you are in an active env. that has this installed) -o is output file and here we are inputting one Query genome at a time with RefGenome
